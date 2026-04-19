709f935a99e83d76e8efed7d22f064c3ff03e80a
j2B237 <yossep237@gmail.com>
2026-04-19 22:54:10 +0000

    everythin is good

--- store-managment-system/app/src/main/kotlin/org/product/Product.kt
+++ store-managment-system/app/src/main/kotlin/org/product/Product.kt
@@ -21,14 +21,19 @@ class Product (var name : String? = null,
     var id : Long = counter.incrementAndGet()
 
     fun infos(){
-        println("Product\n\tName: $name\n\tPrice: ${if (price == null) 0.0 else price} FCFA\n" +
+        println("Product\n\tName: ${if (name == null) "no name" else name}\n" +
+                "\tPrice: ${if (price == null) 0.0 else price} FCFA\n" +
                 "\tStock: ${if (stock == null) 0.0 else stock} item(s)")
     }
 
     /*[Setter]*/
-    fun price(price : Double){
-        if (price < 0)
-            throw IllegalArgumentException ("Price should be positive")
-        this.price(price)
+    fun price (price : Double){
+        if (price < 0) throw IllegalArgumentException ("Price must be positive")
+        this.price = price
+    }
+
+    fun stock (stock : Long){
+        if (stock < 0) throw IllegalArgumentException ("Stock must be positive.")
+        this.stock = stock
     }
 }
