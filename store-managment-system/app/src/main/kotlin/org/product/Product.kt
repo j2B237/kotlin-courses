b2645e28aabca7b6e42177fdf27c28bb5a9d7261
j2B237 <yossep237@gmail.com>
2026-04-19 20:14:09 +0000

    add product package

--- store-managment-system/app/src/main/kotlin/org/product/Product.kt
+++ store-managment-system/app/src/main/kotlin/org/product/Product.kt
@@ -1,4 +1,23 @@
+/**
+ * @file : Product.kt
+ * @author : Yossep BINYOUM
+ * @see : Represent a product object
+ */
+
 package org.product
 
-class Product {
+import java.util.concurrent.atomic.AtomicLong
+
+/*[Default constructor]*/
+class Product (var name : String?,
+    var price : Double?,
+    var stock : Long?) {
+
+    // classe variable
+    companion object {
+        val counter : AtomicLong = AtomicLong(0)
+    }
+
+    var id : Long = counter.incrementAndGet()
+    
 }
